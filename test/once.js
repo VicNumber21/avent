8a5ea79606ee5e4a99fb7d4578b7a594f5dd53e5
Victor Portnov <vic.portnov@gmail.com>
2016-04-20 16:57:14 +0300

    Add 'once' tests

--- test/once.js
+++ test/once.js
@@ -1,6 +1,36 @@
 var expect = require('chai').expect;
-var Avent = require('../avent');
+var Eventified = require('./util/eventified');
+var EventLogger = require('./util/event.logger');
 
 describe('Once', function () {
-  it('TBD');
+  this.timeout(1000);
+  var ctx = {};
+
+  beforeEach(function () {
+    ctx = {
+      e: new Eventified(),
+      logger: new EventLogger()
+    }
+  });
+
+  it('catches single event', function (done) {
+    ctx.e.once('single', ctx.logger.createCallback('single'));
+    ctx.e.trigger('single'); /* ===> */ var expect = {name: 'single', args: []};
+    ctx.e.completeTest(done, ctx.logger,[expect]);
+  });
+
+  it('does not catch events after the first one', function (done) {
+    ctx.e.once('fire', ctx.logger.createCallback('fire'));
+    ctx.e.trigger('fire', 8); /* ===> */ var expect = {name: 'fire', args: [8]};
+    ctx.e.trigger('fire', 500);
+    ctx.e.trigger('fire');
+    ctx.e.trigger('fire', 1, 2, 3, 4, 5, 6);
+    ctx.e.completeTest(done, ctx.logger,[expect]);
+  });
+
+  it('throws error if not initialized properly', function () {
+    delete ctx.e._eventEmitter._eventDispatcher;
+
+    expect(ctx.e.once.bind(ctx.e)).to.throw(Error, /Avent: uninitialized event dispatcher/);
+  });
 });
